57b9303bb437e264fb0a3e021048359c62326a75
0xTovand <tovand@protonmail.com>
2022-11-28 21:26:49 +0300

    change origin

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.set("trust proxy", 1);
 
 app.use(
   cors({
-    origin: "https://chat-backend-kappa.vercel.app/",
+    origin: "https://chat-client-drab.vercel.app",
     // origin: "http://localhost:5173",
   })
 );
@@ -64,46 +64,6 @@ app.post("/messages", async (req, res) => {
   });
   res.json(message);
 });
-
-// app.get("/feed", async (req, res) => {
-//   const posts = await prisma.post.findMany({
-//     where: { published: true },
-//     include: { author: true },
-//   });
-//   res.json(posts);
-// });
-
-// app.post("/post", async (req, res) => {
-//   const { title, content, authorEmail } = req.body;
-//   const post = await prisma.post.create({
-//     data: {
-//       title,
-//       content,
-//       published: false,
-//       author: { connect: { email: authorEmail } },
-//     },
-//   });
-//   res.json(post);
-// });
-
-// app.put("/publish/:id", async (req, res) => {
-//   const { id } = req.params;
-//   const post = await prisma.post.update({
-//     where: { id },
-//     data: { published: true },
-//   });
-//   res.json(post);
-// });
-
-// app.delete("/user/:id", async (req, res) => {
-//   const { id } = req.params;
-//   const user = await prisma.user.delete({
-//     where: {
-//       id,
-//     },
-//   });
-//   res.json(user);
-// });
 
 app.listen(3000, () => console.log(`🚀 Server ready at: 3000`));
 
